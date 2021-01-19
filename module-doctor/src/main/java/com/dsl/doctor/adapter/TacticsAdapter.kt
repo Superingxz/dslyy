178c150008cf35f1662c1eafffa9f7bb9d9390d0
moyaozhi <moyaozhi@aliyun.com>
2021-01-19 17:41:26 +0800

    update:更新依赖

--- module-doctor/src/main/java/com/dsl/doctor/adapter/TacticsAdapter.kt
+++ module-doctor/src/main/java/com/dsl/doctor/adapter/TacticsAdapter.kt
@@ -1,6 +1,5 @@
 package com.dsl.doctor.adapter
 
-import android.content.Context
 import android.widget.ImageView
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.module.LoadMoreModule
@@ -9,7 +8,6 @@ import com.dsl.doctor.R
 import com.dsl.doctor.bean.TacticsBean
 import com.dsl.extend.loadImageExtend
 import com.dsl.util.DateTimeUtil
-import com.umeng.commonsdk.stateless.UMSLEnvelopeBuild.mContext
 
 /**
  * 互联网攻略列表适配器
@@ -20,13 +18,13 @@ class TacticsAdapter(dataList: MutableList<TacticsBean>?) :
     BaseQuickAdapter<TacticsBean, BaseViewHolder>(
         R.layout.doctor_item_tactics, dataList
     ), LoadMoreModule {
-    override fun convert(helper: BaseViewHolder, item: TacticsBean) {
+    override fun convert(holder: BaseViewHolder, item: TacticsBean) {
         item.run {
-            val image = helper.getView<ImageView>(R.id.image)
+            val image = holder.getView<ImageView>(R.id.image)
             image.loadImageExtend(context, top)
-            helper.setText(R.id.title, title)
-            helper.setText(R.id.create_time, DateTimeUtil.getFormtTime(createTime))
-            helper.setText(R.id.read_num, String.format("%d阅读", readNum))
+            holder.setText(R.id.title, title)
+            holder.setText(R.id.create_time, DateTimeUtil.getFormtTime(createTime))
+            holder.setText(R.id.read_num, String.format("%d阅读", readNum))
         }
     }
 }
